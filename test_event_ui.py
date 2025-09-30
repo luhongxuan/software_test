d069f51dbd7f83be9424092e869e8f6c12bf741f
HansLu2025 <B11209013@gapps.ntust.edu.tw>
2025-09-30 23:45:27 +0800

    test, fixes #1

---
+++ test_event_ui.py
@@ -0,0 +1,7 @@
+def test_event_title():
+    title = "Upcoming Events"
+    assert title == "Upcoming Events"
+
+def test_event_list():
+    events = ["Event A - Jan 20", "Event B - Jan 25"]
+    assert len(events) >= 1
